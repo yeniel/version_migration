3471862b1250893d567573e6e7acde421a63ead1
Yeniel Landestoy <y3ni3l@gmail.com>
2020-01-23 14:26:42 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# version_migration
+
+VersionMigration
+===========
+
+This a translation of the library MTMigration (iOS) to a Dart Package.
+
+Manages functions of code that need to run once on version updates in Flutter apps. This could be anything from data
+normalization routines, "What's New In This Version" screens, or bug fixes.
+
+## Installation
+
+MTMigration can be installed one of two ways:
+
+Add in pubspec:
+
+version_migration: ^0.0.1
+
+## Usage
+
+If you need a function that runs every time your application version changes, pass that function to
+the `applicationUpdate` method.
+
+```dart
+VersionMigration.applicationUpdate(() {
+    metrics.resetStats();
+});
+```
+
+If the function is specific to a version, use `migrateToVersion` and VersionMigration will
+ensure that the function is only ever run once for that version.
+
+```objc
+VesionMigration.migrateToVersion("1.1", () {
+    newness.presentNewness();
+});
+```
+
+Because VersionMigration inspects your actual version number and keeps track of the last migration,
+it will migrate all un-migrated functions inbetween. For example, let's say you had the following migrations:
+
+```objc
+VersionMigration migrateToVersion("0.9", () {
+    // Some 0.9 stuff
+});
+
+VersionMigration migrateToVersion("1.0", () {
+    // Some 1.0 stuff
+});
+```
+
+If a user was at version `0.8`, skipped `0.9`, and upgraded to `1.0`, then both the `0.9` *and* `1.0` functions would run.
+
+For debugging/testing purposes, you can call `reset` to clear out the last migration VersionMigration remembered, causing all
+migrations to run from the beginning:
+
+```objc
+VersionMigration.reset();
+```
+
+## Notes
+
+VersionMigration assumes version numbers are incremented in a logical way, i.e. `1.0.1` -> `1.0.2`, `1.1` -> `1.2`, etc.
+
+Version numbers that are past the version specified in your app will not be run. For example, if your pubspec file
+specifies `1.2` as the app's version number, and you attempt to migrate to `1.3`, the migration will not run.
+
+## Contributing
+
+This library does not handle some more intricate migration situations, if you come across intricate use cases from your own
+app, please add it and submit a pull request. Be sure to add test cases.
---
+++ lib/version_migration.dart
@@ -0,0 +1,113 @@
+library version_migration;
+
+import 'dart:math';
+
+import 'package:flutter/foundation.dart';
+import 'package:package_info/package_info.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class VersionMigration {
+
+  static String lastMigratedVersionKey = "Migrator.lastMigratedVersionKey";
+  static String lastUpdatedAppVersionKey = "Migrator.lastUpdatedAppVersionKey";
+
+  static Future<void> migrateToVersion(String version, Function migrationFunction) async {
+    Version newVersion = Version(version: version);
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    Version appVersion = Version(version: packageInfo.version);
+    Version lastMigratedVersion = await getLastMigratedVersion();
+
+    if (newVersion.compareTo(lastMigratedVersion) == 1 && newVersion.compareTo(appVersion) == 1) {
+      migrationFunction();
+      setLastMigratedVersion(version.toString());
+    }
+  }
+
+  static Future<void> applicationUpdate(Function updateFunction) async {
+    Version lastUpdatedAppVersion = await getLastUpdatedAppVersion();
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+
+    if (lastUpdatedAppVersion.toString() != packageInfo.version) {
+      updateFunction();
+      setLastUpdatedAppVersion(packageInfo.version);
+    }
+  }
+
+  static reset() {
+    setLastMigratedVersion(null);
+    setLastUpdatedAppVersion(null);
+  }
+
+  static Future<Version> getLastMigratedVersion() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    return Version(version: prefs.getString(lastMigratedVersionKey));
+  }
+
+  static Future<bool> setLastMigratedVersion(String value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    return prefs.setString(lastMigratedVersionKey, value);
+  }
+
+  static Future<Version> getLastUpdatedAppVersion() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    return Version(version: prefs.getString(lastUpdatedAppVersionKey));
+  }
+
+  static Future<bool> setLastUpdatedAppVersion(String value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    return prefs.setString(lastUpdatedAppVersionKey, value);
+  }
+}
+
+class Version implements Comparable<Version> {
+  final String version;
+
+  Version({@required this.version}) {
+    if (version == null) {
+      throw FormatException();
+    } else {
+      RegExp versionRegExp = new RegExp(r"[0-9]+(\\.[0-9]+)*");
+
+      if (!versionRegExp.hasMatch(version)) {
+        throw FormatException();
+      }
+    }
+  }
+
+  @override
+  int compareTo(Version other) {
+    if (other == null) {
+      return 1;
+    } else {
+      List<String> versionParts = version.split(".");
+      List<String> otherParts = other.version.split(".");
+
+      int numberOfParts = max(versionParts.length, otherParts.length);
+
+      for (var index = 0; index < numberOfParts; index++) {
+        int versionPart = index < versionParts.length ? versionParts[index] : 0;
+        int otherPart = index < otherParts.length ? otherParts[index] : 0;
+
+        if (versionPart < otherPart) {
+          return -1;
+        }
+
+        if (versionPart > otherPart) {
+          return 1;
+        }
+      }
+
+      return 0;
+    }
+  }
+
+  @override
+  String toString() {
+    return version;
+  }
+
+}
