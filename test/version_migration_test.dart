1cfa50eae78c4490fbdde360e59566456da3a8e4
Yeniel Landestoy <y3ni3l@gmail.com>
2020-01-23 18:04:24 +0100

    Tests for reset method

--- test/version_migration_test.dart
+++ test/version_migration_test.dart
@@ -1,13 +1,69 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:package_info/package_info.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:version_migration/version_migration.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    SharedPreferences.setMockInitialValues({});
+    mockPackageInfo();
+    VersionMigration.reset();
   });
+
+  tearDown(() {
+
+  });
+
+  test('migration reset', () async {
+    await givenAFirstMigration();
+    await whenResetVersionMigration();
+    await whenMigrateAgain();
+    expectMigrationIsDoneAgain();
+  });
+
+}
+
+bool migration09Done = false;
+bool migration10Done = false;
+
+void mockPackageInfo() {
+  const MethodChannel('plugins.flutter.io/package_info').setMockMethodCallHandler((MethodCall methodCall) async {
+    if (methodCall.method == 'getAll') {
+      return <String, dynamic>{
+        'appName': 'VersionMigration',
+        'packageName': 'VersionMigration',
+        'version': '0.0.0',
+        'buildNumber': '0'
+      };
+    }
+    return null;
+  });
+}
+
+givenAFirstMigration() async {
+  await VersionMigration.migrateToVersion("0.9", () {});
+  await VersionMigration.migrateToVersion("1.0", () {});
+}
+
+whenResetVersionMigration() {
+  VersionMigration.reset();
+}
+
+whenMigrateAgain() async {
+  await VersionMigration.migrateToVersion("0.9", () {
+    migration09Done = true;
+  });
+
+  await VersionMigration.migrateToVersion("1.0", () {
+    migration10Done = true;
+  });
+}
+
+expectMigrationIsDoneAgain() {
+  expect(migration09Done, true);
+  expect(migration10Done, true);
 }
