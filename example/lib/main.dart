9765bf46679ac07287a38a4270d5642753618e75
Yeniel Landestoy <y3ni3l@gmail.com>
2021-05-05 01:36:25 +0200

    Example was migrated to null-safety

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,9 +13,9 @@ class MyApp extends StatelessWidget {
       String newLastSavedCounterKey = "newLastSavedCounterKey";
 
       SharedPreferences.getInstance().then((sharedPreferences) {
-        int counter = sharedPreferences.getInt(oldLastSavedCounterKey);
+        int? counter = sharedPreferences.getInt(oldLastSavedCounterKey);
 
-        sharedPreferences.setInt(newLastSavedCounterKey, counter);
+        sharedPreferences.setInt(newLastSavedCounterKey, counter ?? 0);
       });
     });
 
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key? key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -50,14 +50,14 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
+  final String? title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  SharedPreferences sharedPreferences;
+  late SharedPreferences sharedPreferences;
 
   // Version 1.0.0
   String lastSavedCounterKey = "oldLastSavedCounterKey";
@@ -100,7 +100,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(widget.title ?? ""),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
